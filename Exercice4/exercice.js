9d6dfe8ba2336941459aaf3e4d212007595ffdda
nabil-leroy <nabileroy@yahoo.fr>
2017-02-20 11:55:02 +0100

    Add files via upload
    Refaire l’exercice 2 en demandant le nom, le prénom et la ville à l’utilisateur.

---
+++ Exercice4/exercice.js
@@ -0,0 +1,4 @@
+var name =prompt('Entrez votre prénom :');
+var firstname=prompt('Entrez votre nom :');
+var city =prompt('Entrez votre ville :');
+alert("Nom : "+name+"\nPrénom : "+firstname+"\nVille : "+city);
